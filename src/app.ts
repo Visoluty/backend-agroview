e3faf0eb0bc39ce9f6f4a0eed6fafe4804e00a92
LuisGustavo <luisgustavobarbosa43@gmail.com>
2025-10-25 17:09:10 -0300

    Removido cors

--- src/app.ts
+++ src/app.ts
@@ -16,13 +16,15 @@ import { swaggerSpec } from './config/swagger';
 
 const app = express();
 
+app.use(cors());
+
 // Middlewares globais
-app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
-    ? process.env.FRONTEND_URL 
-    : ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:5173'],
-  credentials: true
-}));
+// app.use(cors({
+//   origin: process.env.NODE_ENV === 'production' 
+//     ? process.env.FRONTEND_URL 
+//     : ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:5173'],
+//   credentials: true
+// }));
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
